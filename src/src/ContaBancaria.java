c6dfe14479c73134ea038aec1de3ad784fce5dfd
Gato Fantasma <Luized3859@gmail.com>
2026-07-09 18:29:57 -0300

    começo
    definição dos atributos iniciais

---
+++ src/src/ContaBancaria.java
@@ -0,0 +1,14 @@
+public class ContaBancaria {
+    public class Dados{
+
+        String nome;
+        double saldo;
+        int agencia;
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
