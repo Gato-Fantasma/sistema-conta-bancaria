5f2ff07da5f6f1ce332030207709f0afb0ec79de
Luiz_Eduardo38 <162372035+Gato-Fantasma@users.noreply.github.com>
2026-07-10 16:58:32 -0300

    Update ContaBancaria.java

--- src/src/ContaBancaria.java
+++ src/src/ContaBancaria.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ContaBancaria {
     public class Dados{
 
@@ -5,10 +7,23 @@ public class ContaBancaria {
         double saldo;
         int agencia;
 
+        Scanner scanner = new Scanner(System.in);
+
 
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.println("Ola, Insira seus dados adiante");
+
+        System.out.println("Nome Completo:")
+        nome = scanner.nextLine();
+
+        System.out.println("Número da Agência:")
+        nome = scanner.nextInt();
+
+        System.out.println("Saldo a Depositar:")
+        nome = scanner.nextFloat();
+
     }
 }
