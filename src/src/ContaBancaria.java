622a3b3e796752dde51981d4781b7a9ab97f95dc
Gato Fantasma <Luized3859@gmail.com>
2026-07-14 18:14:58 -0300

    Update ContaBancaria.java

--- src/src/ContaBancaria.java
+++ src/src/ContaBancaria.java
@@ -12,18 +12,60 @@ public class ContaBancaria {
 
     }
 
+    public void Consulta(){
+
+        System.out.println("Qual dado deseja consultar?");
+        System.out.println("1-nome");
+        System.out.println("2-saldo");
+        System.out.println("3-agencia");
+        int dado = scanner.nextInt();
+
+        switch(dado){
+            switch (dado) {
+                case 1:
+                    System.out.println(nome);
+                    break;
+                case 2:
+                    System.out.println(saldo);
+                    break;
+                case 3:
+                    System.out.println(agencia);
+                    break;
+                default:
+                    System.out.println("Opção inválida"); // Se nenhum caso for atendido
+        }
+
+
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Ola, Insira seus dados adiante");
 
-        System.out.println("Nome Completo:")
+        System.out.println("Nome Completo:");
         nome = scanner.nextLine();
 
-        System.out.println("Número da Agência:")
-        nome = scanner.nextInt();
+        System.out.println("Número da Agência:");
+        agencia = scanner.nextInt();
 
-        System.out.println("Saldo a Depositar:")
-        nome = scanner.nextFloat();
+        System.out.println("Saldo a Depositar:");
+        saldo = scanner.nextFloat();
+
+        System.out.println("Deseja consultar algum dado?");
+        String opcao = scanner.nextLine();
+
+        switch (opcao) {
+            case "sim":
+                Consulta();
+                break;
+            case "não":
+                "programa encerrado"
+                break;
+        }
+
+
+
+
 
     }
 }
